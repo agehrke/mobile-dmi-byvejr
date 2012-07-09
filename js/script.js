b6eec3a195994a6dae5fb41571f10991ed1f60e0
agehrke <andreas.gehrke@enabling.dk>
2012-07-09 21:14:55 +0200

    Display city name from Geoservicen

--- js/script.js
+++ js/script.js
@@ -3,18 +3,22 @@ $(function() {
 		$(".byvejr").addClass("active");
 		$(".two-day-forecast").attr("src", "http://servlet.dmi.dk/byvejr/servlet/byvejr_dag1?by="+ zipCode+"&mode=long");
 		$(".nine-day-forecast").attr("src", "http://servlet.dmi.dk/byvejr/servlet/byvejr?by="+ zipCode+"&tabel=dag3_9");
+		
+		$.getJSON("http://geo.oiorest.dk/postnumre/"+ zipCode +".json?callback=?", function(data) {
+			$("title, h1").text("DMI Byvejr - " + data.navn);
+		});
 	}
 	
 	if (window.location.hash) {
 		var zipCode = window.location.hash.replace("#", "");
 		$(".zipcode").val(zipCode);
-		displayForecasts(zipCode);
+		displayForecasts(zipCode);		
 	}
 
 	$(".zipcode-form").submit(function() {
 		var zipCode = $(".zipcode").val();
 		displayForecasts(zipCode);
-		window.location.hash = zipCode;
+		window.location.hash = zipCode;		
 		return false;
 	});
 });
