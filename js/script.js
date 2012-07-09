ea110a8cf55d14646da145bced41a4a88a2a74d1
agehrke <andreas.gehrke@enabling.dk>
2012-07-09 20:35:32 +0200

    Deleted unnecessary files

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
-$(function() {
-	$(".zipcode-form").submit(function() {
-		var zipcode = $(".zipcode").val();
+$(function() {		
+	var displayForecasts = function(zipCode) {
 		$(".byvejr").addClass("active");
-		$(".two-day-forecast").attr("src", "http://servlet.dmi.dk/byvejr/servlet/byvejr_dag1?by="+ zipcode+"&mode=long");
-		$(".two-day-forecast").attr("src", "http://servlet.dmi.dk/byvejr/servlet/byvejr?by="+ zipcode+"&tabel=dag3_9");
+		$(".two-day-forecast").attr("src", "http://servlet.dmi.dk/byvejr/servlet/byvejr_dag1?by="+ zipCode+"&mode=long");
+		$(".nine-day-forecast").attr("src", "http://servlet.dmi.dk/byvejr/servlet/byvejr?by="+ zipCode+"&tabel=dag3_9");
+	}
+	
+	if (window.location.hash) {
+		var zipCode = window.location.hash.replace("#", "");
+		$(".zipcode").val(zipCode);
+		displayForecasts(zipCode);
+	}
+
+	$(".zipcode-form").submit(function() {
+		var zipCode = $(".zipcode").val();
+		displayForecasts(zipCode);
+		window.location.hash = zipCode;
 		return false;
 	});
 });
