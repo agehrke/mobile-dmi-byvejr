f15d44efe20e8f0f7210633906d2ed14154a9e0a
agehrke <ag@agehrke.com>
2012-07-13 12:49:18 +0200

    Scroll to forecasts

--- js/script.js
+++ js/script.js
@@ -1,24 +1,29 @@
 $(function() {		
-	var displayForecasts = function(zipCode) {
+	var displayForecasts = function(zipCode, callback) {
 		$(".byvejr").addClass("active");
 		$(".two-day-forecast").attr("src", "http://servlet.dmi.dk/byvejr/servlet/byvejr_dag1?by="+ zipCode+"&mode=long");
 		$(".nine-day-forecast").attr("src", "http://servlet.dmi.dk/byvejr/servlet/byvejr?by="+ zipCode+"&tabel=dag3_9");
 		
 		$.getJSON("http://geo.oiorest.dk/postnumre/"+ zipCode +".json?callback=?", function(data) {
 			$("title, h1").text("DMI Byvejr - " + data.navn);
+			if ($.isFunction(callback)) callback();
 		});
+	}
+	
+	var scrollToForecasts = function() {
+		$('html, body').animate({scrollTop: $(".byvejr").offset().top}, 500);
 	}
 	
 	if (window.location.hash) {
 		var zipCode = window.location.hash.replace("#", "");
 		$(".zipcode").val(zipCode);
-		displayForecasts(zipCode);		
+		displayForecasts(zipCode, scrollToForecasts);
 	}
 
 	$(".zipcode-form").submit(function() {
 		var zipCode = $(".zipcode").val();
-		displayForecasts(zipCode);
-		window.location.hash = zipCode;		
+		displayForecasts(zipCode, scrollToForecasts);
+		window.location.hash = zipCode;
 		return false;
 	});
 });
