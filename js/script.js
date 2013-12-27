392787c253fbd8a149f185d9c55acb3927846ce0
agehrke <ag@agehrke.com>
2013-12-27 14:26:17 +0100

    Tilføjet yr.no 2-døgnsudsigt.

--- js/script.js
+++ js/script.js
@@ -9,18 +9,83 @@
 
 // DOM ready
 $(function() {
+	var yrNo = (function() {
+		var zipCodes = {
+			"1000" : "Capital/Copenhagen",
+			"2670" : "Zealand/Greve/",
+			"2970" : "Capital/Hørsholm/",
+			"2630" : "Capital/Taastrup/",
+			"3000" : "Capital/Elsinore/",
+			"3250" : "Capital/Smidstrup~2613357/",
+			"3400" : "Capital/Hillerød/",			
+			"3700" : "Capital/Rønne/",
+			"4000" : "Zealand/Roskilde/",						
+			"4180" : "Zealand/Sorø/",
+			"4200" : "Zealand/Slagelse/",			
+			"4600" : "Zealand/Køge/",
+			"4700" : "Zealand/Næstved/",
+			"5000" : "South_Denmark/Odense/",
+			"5700" : "South_Denmark/Svendborg/",
+			"6000" : "South_Denmark/Kolding/",
+			"6400" : "South_Denmark/Sønderborg/",
+			"6700" : "South_Denmark/Esbjerg/",
+			"7000" : "South_Denmark/Fredericia/",
+			"7100" : "South_Denmark/Vejle/",
+			"7700" : "North_Jutland/Thisted/",
+			"7400" : "Central_Jutland/Herning/",
+			"7500" : "Central_Jutland/Holstebro/",
+			"7800" : "Central_Jutland/Skive/",
+			"7900" : "North_Jutland/Nykøbing_Mors/",						
+			"9000" : "North_Jutland/Aalborg/",
+			"9500" : "North_Jutland/Hobro/",
+			"9700" : "North_Jutland/Brønderslev/",
+			"9900" : "North_Jutland/Frederikshavn/",
+			"9800" : "North_Jutland/Hjørring/",
+			"8000" : "Central_Jutland/Aarhus/",
+			"8620" : "Central_Jutland/Kjellerup/",
+			"8700" : "Central_Jutland/Horsens/",
+			"8600" : "Central_Jutland/Silkeborg/",
+			"8722" : "Central_Jutland/Hedensted/",
+			"8800" : "Central_Jutland/Viborg/",
+			"8850" : "Central_Jutland/Bjerringbro/",
+			"8900" : "Central_Jutland/Randers/",
+		};
+
+		var findNameByZipCode = function(zipCode) {
+			if (zipCodes[zipCode]) return zipCodes[zipCode];
+			// Change last two digits to zeros
+			zipCode[3] = "0"; replace(/.{2}$/, "00");
+			if (zipCodes[zipCode]) return zipCodes[zipCode];
+			else return null;
+		}
+
+		var getHourlyForecastImageUrl = function(name) {
+			return "http://www.yr.no/place/Denmark/"+ name +"/meteogram.png";
+		}
+
+		return {
+			'getHourlyForecastImageUrl': function(zipCode) {
+				var name = findNameByZipCode(zipCode);
+				if (name != null) return getHourlyForecastImageUrl(name);
+				else null;
+			},
+
+			'hasForecastFor': function(zipCode) {
+				return findNameByZipCode(zipCode) != null;
+			}
+		};
+	}());
+
 	var displayForecasts = function(zipCode, callback) {
-		$(".byvejr").addClass("active loading");
-		$(".two-day-forecast").attr("src", "http://servlet.dmi.dk/byvejr/servlet/byvejr_dag1?by="+ zipCode +"&mode=long");
-		$(".nine-day-forecast").attr("src", "http://servlet.dmi.dk/byvejr/servlet/byvejr?by="+ zipCode +"&tabel=dag3_9");
+		$(".byvejr").addClass("active");
+
+		var container = $(".two-day-forecast");
+		var imageUrl = "http://servlet.dmi.dk/byvejr/servlet/byvejr_dag1?by="+ zipCode +"&mode=long";
+		displayForecast(container, imageUrl, callback);
 		
-		// Capture load of forecast images
-		var dfd = $(".byvejr").imagesLoaded();
-		dfd.done(function(images) {
-			$(".byvejr").removeClass("loading");
-			if ($.isFunction(callback)) callback();
-		});
-		
+		// Reload previously loaded forecasts
+		$(".forecast-container.loaded .activator").trigger("click");
+
 		// Get city name from zip code using Geoservicen
 		$.getJSON("http://geo.oiorest.dk/postnumre/"+ zipCode +".json?callback=?", function(data) {
 			$("title, h1").text(data.navn);
@@ -61,6 +126,17 @@ $(function() {
 			});
 		});
 	};
+
+	var displayForecast = function(container, imageUrl, callback) {
+		container.addClass("loading").removeClass("loaded");
+		container.find(".forecast").attr("src", imageUrl);
+		
+		var dfd = container.imagesLoaded();
+		dfd.done(function(images) {
+			container.removeClass("loading").addClass("loaded");
+			if ($.isFunction(callback)) callback();
+		});
+	}
 	
 	// Handle click on radar link
 	$(".radar-link").click(function() { 
@@ -75,6 +151,28 @@ $(function() {
 		window.location.hash = zipCode;
 		ga('send', 'event', 'Forecast', 'Show', 'Form submit', zipCode);
 		return false;
+	});
+
+	$(".two-day-forecast-yr .activator").on("click", function() {
+		var container = $(this).parents(".forecast-container");
+		var zipCode = $(".zipcode").val();
+		var imageUrl = yrNo.getHourlyForecastImageUrl(zipCode);
+
+		displayForecast(container, imageUrl);
+	});
+
+	$(".two-day-forecast .activator").on("click", function() {
+		var container = $(this).parents(".forecast-container");
+		var zipCode = $(".zipcode").val();
+		var imageUrl = "http://servlet.dmi.dk/byvejr/servlet/byvejr_dag1?by="+ zipCode +"&mode=long";
+		displayForecast(container, imageUrl);
+	});
+
+	$(".nine-day-forecast .activator").on("click", function() {
+		var container = $(this).parents(".forecast-container");
+		var zipCode = $(".zipcode").val();
+		var imageUrl = "http://servlet.dmi.dk/byvejr/servlet/byvejr?by="+ zipCode +"&tabel=dag3_9";
+		displayForecast(container, imageUrl);
 	});
 	
 	// Check location hash for zip code etc
