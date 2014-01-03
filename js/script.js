82af79e2ad9c5a14c6d6515fe04ea1b9164be4e6
agehrke <ag@agehrke.com>
2014-01-03 13:49:19 +0100

    Implemented zoomable forecasts.

--- js/script.js
+++ js/script.js
@@ -1,15 +1,14 @@
-﻿/*!
+/*!
  * jQuery imagesLoaded plugin v2.0.1
  * http://github.com/desandro/imagesloaded
  *
  * MIT License. by Paul Irish et al.
  */
-(function(c,n){var k="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==";c.fn.imagesLoaded=function(l){function m(){var b=c(h),a=c(g);d&&(g.length?d.reject(e,b,a):d.resolve(e));c.isFunction(l)&&l.call(f,e,b,a)}function i(b,a){b.src===k||-1!==c.inArray(b,j)||(j.push(b),a?g.push(b):h.push(b),c.data(b,"imagesLoaded",{isBroken:a,src:b.src}),o&&d.notifyWith(c(b),[a,e,c(h),c(g)]),e.length===j.length&&(setTimeout(m),e.unbind(".imagesLoaded")))}var f=this,d=c.isFunction(c.Deferred)?c.Deferred():
-0,o=c.isFunction(d.notify),e=f.find("img").add(f.filter("img")),j=[],h=[],g=[];e.length?e.bind("load.imagesLoaded error.imagesLoaded",function(b){i(b.target,"error"===b.type)}).each(function(b,a){var e=a.src,d=c.data(a,"imagesLoaded");if(d&&d.src===e)i(a,d.isBroken);else if(a.complete&&a.naturalWidth!==n)i(a,0===a.naturalWidth||0===a.naturalHeight);else if(a.readyState||a.complete)a.src=k,a.src=e}):m();return d?d.promise(f):f}})(jQuery);
+(function(c,n){var k="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==";c.fn.imagesLoaded=function(l){function m(){var b=c(h),a=c(g);d&&(g.length?d.reject(e,b,a):d.resolve(e));c.isFunction(l)&&l.call(f,e,b,a)}function i(b,a){b.src===k||-1!==c.inArray(b,j)||(j.push(b),a?g.push(b):h.push(b),c(b).data("imagesLoaded",{isBroken:a,src:b.src}),o&&d.notifyWith(c(b),[a,e,c(h),c(g)]),e.length===j.length&&(setTimeout(m),e.unbind(".imagesLoaded")))}var f=this,d=c.isFunction(c.Deferred)?c.Deferred():
+0,o=c.isFunction(d.notify),e=f.find("img").add(f.filter("img")),j=[],h=[],g=[];e.length?e.bind("load.imagesLoaded error.imagesLoaded",function(b){i(b.target,"error"===b.type)}).each(function(b,a){var e=a.src,d=c(a).data("imagesLoaded");if(d&&d.src===e)i(a,d.isBroken);else if(a.complete&&a.naturalWidth!==n)i(a,0===a.naturalWidth||0===a.naturalHeight);else if(a.readyState||a.complete)a.src=k,a.src=e}):m();return d?d.promise(f):f}})(jQuery);
 
-// DOM ready
-$(function() {
-	var yrNo = (function() {
+// Yr.no wrapper
+var yrNo = (function() {
 		var zipCodes = {
 			"1000" : "Capital/Copenhagen",
 			"2670" : "Zealand/Greve/",
@@ -54,7 +53,7 @@ $(function() {
 		var findNameByZipCode = function(zipCode) {
 			if (zipCodes[zipCode]) return zipCodes[zipCode];
 			// Change last two digits to zeros
-			zipCode[3] = "0"; replace(/.{2}$/, "00");
+			zipCode = zipCode.replace(/.{2}$/, "00");
 			if (zipCodes[zipCode]) return zipCodes[zipCode];
 			else return null;
 		}
@@ -76,27 +75,80 @@ $(function() {
 		};
 	}());
 
-	var displayForecasts = function(zipCode, callback) {
+function Forecast(container, imageUrlFunction) {
+	var self = this;
+	self.container = container;
+	self.imageUrlFunction = imageUrlFunction;
+
+	this.display = function(zipCode, callback) {
+		self.container.addClass("loading").removeClass("loaded");
+
+		var imageUrl = imageUrlFunction(zipCode);
+		self.container.find(".forecast").attr("src", imageUrl);
+		
+		var dfd = container.imagesLoaded();
+		dfd.done(function(images) {
+			self.container.removeClass("loading").addClass("loaded");
+			if ($.isFunction(callback)) callback();
+		});
+	};
+
+	this.toggleScrollable = function() {
+		self.container.toggleClass("scrollable");
+	}
+
+	this.isLoaded = function() {
+		return self.container.hasClass("loaded");
+	};
+}
+
+function ByvejrModel() {
+	var self = this;
+	self.zipCode = "";
+	self.forecasts = {};
+
+	// Create forecasts
+	self.forecasts["dmi-2"] = new Forecast($(".two-day-forecast"), function(zipCode) {
+									return "http://servlet.dmi.dk/byvejr/servlet/byvejr_dag1?by="+ zipCode +"&mode=long";
+								});
+	
+	self.forecasts["dmi-9"] = new Forecast($(".nine-day-forecast"), function(zipCode) {
+									return "http://servlet.dmi.dk/byvejr/servlet/byvejr?by="+ zipCode +"&tabel=dag3_9";
+								});
+
+	self.forecasts["yr-2"] = new Forecast($(".two-day-forecast-yr"), yrNo.getHourlyForecastImageUrl);
+
+	this.displayForecasts = function(zipCode, callback) {
+		self.zipCode = zipCode;
+
 		$(".byvejr").addClass("active");
 
-		var container = $(".two-day-forecast");
-		var imageUrl = "http://servlet.dmi.dk/byvejr/servlet/byvejr_dag1?by="+ zipCode +"&mode=long";
-		displayForecast(container, imageUrl, callback);
+		// Display DMI 2-day forecast
+		self.forecasts["dmi-2"].display(zipCode, callback);
 		
 		// Reload previously loaded forecasts
-		$(".forecast-container.loaded .activator").trigger("click");
+		$.each(self.forecasts, function(i, forecast) {
+			if (forecast.isLoaded()) forecast.display(zipCode);
+		});
 
 		// Get city name from zip code using Geoservicen
 		$.getJSON("http://geo.oiorest.dk/postnumre/"+ zipCode +".json?callback=?", function(data) {
 			$("title, h1").text(data.navn);
 		});
-	}
-	
-	var scrollToForecasts = function() {
-		$('html, body').animate({scrollTop: $(".byvejr").offset().top}, 500);
-	}
-	
-	var showRadar = function() {
+	};
+
+	this.getCurrentZipCodeFromUserLocation = function(callback){
+		navigator.geolocation.getCurrentPosition(function(position) {
+			var latLng = position.coords.latitude + "," + position.coords.longitude;
+			$.getJSON("http://geo.oiorest.dk/postnumre/"+ latLng +".json?callback=?", function(data) {
+				callback(data.fra);
+			}, function(error) {
+				console.warn("Failed to get current position", error);
+			});
+		});
+	};
+
+	this.showRadar = function() {
 		$(".radar").addClass("active");
 		$('html, body').animate({scrollTop: $(".radar").offset().top}, 500);		
 		$.getJSON("http://query.yahooapis.com/v1/public/yql/agehrke/nedboers-radar2?format=json&callback=?", function(data) {		
@@ -114,77 +166,57 @@ $(function() {
 				alert("Kunne ikke hente radar-billeder fra DMI. Prøv igen senere.");
 			}
 		});
-	}
-	
-	var getCurrentZipCodeFromUserLocation = function(callback){
-		navigator.geolocation.getCurrentPosition(function(position) {
-			var latLng = position.coords.latitude + "," + position.coords.longitude;
-			$.getJSON("http://geo.oiorest.dk/postnumre/"+ latLng +".json?callback=?", function(data) {
-				callback(data.fra);
-			}, function(error) {
-				console.warn("Failed to get current position", error);
-			});
-		});
 	};
 
-	var displayForecast = function(container, imageUrl, callback) {
-		container.addClass("loading").removeClass("loaded");
-		container.find(".forecast").attr("src", imageUrl);
-		
-		var dfd = container.imagesLoaded();
-		dfd.done(function(images) {
-			container.removeClass("loading").addClass("loaded");
-			if ($.isFunction(callback)) callback();
-		});
-	}
-	
+	this.scrollToForecasts = function() {
+		$('html, body').animate({scrollTop: $(".byvejr").offset().top}, 500);
+	};	
+};
+
+// DOM ready
+$(function() {
+	var model = new ByvejrModel();
+
 	// Handle click on radar link
 	$(".radar-link").click(function() { 
 		ga('send', 'event', 'Radar', 'Show', 'Link');
-		showRadar();
+		model.showRadar();
 	});
 
 	// Handle form submit of zip code
 	$(".zipcode-form").submit(function() {
 		var zipCode = $(".zipcode").val();
-		displayForecasts(zipCode, scrollToForecasts);
+		model.displayForecasts(zipCode, model.scrollToForecasts);
+		
 		window.location.hash = zipCode;
 		ga('send', 'event', 'Forecast', 'Show', 'Form submit', zipCode);
 		return false;
 	});
 
-	$(".two-day-forecast-yr .activator").on("click", function() {
-		var container = $(this).parents(".forecast-container");
-		var zipCode = $(".zipcode").val();
-		var imageUrl = yrNo.getHourlyForecastImageUrl(zipCode);
 
-		displayForecast(container, imageUrl);
+	$(".forecast-container")
+	// Display forecast when clicking activator
+	.on("click", ".activator", function(e) {
+		var name = $(e.delegateTarget).data("name");
+		model.forecasts[name].display(model.zipCode);
+	})
+	// Toogle scrollable when clicking on forecast
+	.on("click", ".forecast", function(e) {
+		var name = $(e.delegateTarget).data("name");
+		model.forecasts[name].toggleScrollable();
 	});
-
-	$(".two-day-forecast .activator").on("click", function() {
-		var container = $(this).parents(".forecast-container");
-		var zipCode = $(".zipcode").val();
-		var imageUrl = "http://servlet.dmi.dk/byvejr/servlet/byvejr_dag1?by="+ zipCode +"&mode=long";
-		displayForecast(container, imageUrl);
-	});
-
-	$(".nine-day-forecast .activator").on("click", function() {
-		var container = $(this).parents(".forecast-container");
-		var zipCode = $(".zipcode").val();
-		var imageUrl = "http://servlet.dmi.dk/byvejr/servlet/byvejr?by="+ zipCode +"&tabel=dag3_9";
-		displayForecast(container, imageUrl);
-	});
+	
 	
 	// Check location hash for zip code etc
 	if (window.location.hash) {
 		if (window.location.hash == "#radar") {
 			ga('send', 'event', 'Radar', 'Show', 'Direct link');
-			showRadar();
+			model.showRadar();
 		} else if (window.location.hash == "#min-position") {
 			if ("geolocation" in navigator) {
-				getCurrentZipCodeFromUserLocation(function(zipCode) {
+				model.getCurrentZipCodeFromUserLocation(function(zipCode) {
 					$(".zipcode").val(zipCode);
-					displayForecasts(zipCode, scrollToForecasts);
+					model.displayForecasts(zipCode, model.scrollToForecasts);
 				});
 			} else {
 				$("title, h1").text("Fejl i udtræk af position").addClass("error");
@@ -193,8 +225,9 @@ $(function() {
 		}
 		else {
 			var zipCode = window.location.hash.replace("#", "");
-			$(".zipcode").val(zipCode);
-			displayForecasts(zipCode, scrollToForecasts);
+			$(".zipcode").val(zipCode);			
+
+			model.displayForecasts(zipCode, model.scrollToForecasts);
 			ga('send', 'event', 'Forecast', 'Show', 'Direct link', zipCode);
 		}
 	}
